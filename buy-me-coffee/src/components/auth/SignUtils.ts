fdaf6b5cac20e72a111f6a29d19f4b0640355fec
URSA0112 <Nimone99@gmail.com>
2025-04-30 16:11:42 +0800

    Signin Fixed really , Just schema changed

--- buy-me-coffee/src/components/auth/SignUtils.ts
+++ buy-me-coffee/src/components/auth/SignUtils.ts
@@ -14,7 +14,7 @@ export const SignUpSchema = z.object({
 
 
 export const SignInSchema = z.object({
-    identify: z.string().email("Please enter a valid email"),
+    identify: z.string().min(2, "Please enter a valid email"),
     password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
